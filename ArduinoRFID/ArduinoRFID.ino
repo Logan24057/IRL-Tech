b7a5eba74eb72b1befcf17221a2de8e549107d5c
Logan24057 <82458583+Logan24057@users.noreply.github.com>
2024-02-08 11:02:02 -0500

    Reads, writes, and erases records

--- ArduinoRFID/ArduinoRFID.ino
+++ ArduinoRFID/ArduinoRFID.ino
@@ -3,26 +3,73 @@
 #include <PN532_SPI.h>
 #include <PN532.h>
 #include <NfcAdapter.h>
-PN532_SPI interface(SPI, 10); // create a PN532 SPI interface with the SPI CS terminal located at digital pin 10
-NfcAdapter nfc = NfcAdapter(interface); // create an NFC adapter object
+#include <NdefMessage.h>
 
-
+PN532_SPI interface(SPI, 10);            // create a PN532 SPI interface with the SPI CS terminal located at digital pin 10
+NfcAdapter nfc = NfcAdapter(interface);  // create an NFC adapter object
 String tagId = "None";
-
+NdefMessage message = NdefMessage();     // createa a message object
+unsigned int count = message.getRecordCount(); // get the number of records
 void setup(void) {
+  //serial.write and serial.send
   Serial.begin(115200);
   Serial.println("System initialized");
 
+
   nfc.begin();
+  Serial.print("\n");
+  Serial.print("Here are some commands that can be used:");
+  Serial.print("\n");
+  Serial.print("read");
+  Serial.print("\n");
+  Serial.print("write");
+  Serial.print("\n");
+  Serial.print("erase");
+  Serial.print("\n");
+  Serial.print("\n");
 }
 
 void loop() {
-  readNFC();
+
+   while (Serial.available() > 0) {
+    String inputString = Serial.readString();  // Read the incoming data as a string
+    inputString.trim();                        // Remove leading and trailing whitespaces
+
+    if (inputString.equals("read")) {  // Compare the received string with "read"
+      readNFC();                       // Do something if the received string is "read"
+      Serial.print(count);
+    }
+    else if (inputString.equals("erase")){
+      nfc.erase();
+    }
+    else if (inputString.equals("write")) {
+      Serial.print("\n");
+      Serial.println("What would you like to write?");
+
+      while (!Serial.available()) {
+        // Wait for user input
+      }
+      
+      String writeString = Serial.readString();  // Read the incoming data as a string
+      writeString.trim();                        // Remove leading and trailing whitespaces
+      
+      Serial.println(writeString);
+
+      message.addTextRecord(writeString);
+      message.addTextRecord(writeString);
+
+      boolean success = nfc.write(message);
+      if (success) {
+        Serial.println("Success. Try reading this tag with your phone.");
+      } else {
+        Serial.println("Write failed");
+      }
+    }
+  }
 }
 
 void readNFC() {
-  if (nfc.tagPresent())
-  {
+  if (nfc.tagPresent()) {
     NfcTag tag = nfc.read();
     tag.print();
     tagId = tag.getUidString();
